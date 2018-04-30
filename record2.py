e4a9c7634ea789d348461bf8435cc46a980a66b2
nienna73 <jcpoulin@ualberta.ca>
2018-04-30 23:06:38 +0000

    Added loop to record file

--- record2.py
+++ record2.py
@@ -4,6 +4,7 @@ import time
 video_length = 3
 # 29 minutes = 1740
 camera_ports = []
+x = 0
 
 ports_string = subprocess.check_output(["gphoto2", "--auto-detect"])
 ports_string_split = ports_string.split()
@@ -11,11 +12,14 @@ for item in ports_string_split:
     if item[0] == 'u':
         camera_ports.append(item)
 
-for port in camera_ports:
-    subprocess.call(["gphoto2", "--port=" + port, "--set-config", "movie=1"])
+while (x < 3):
+    for port in camera_ports:
+        subprocess.call(["gphoto2", "--port=" + port, "--set-config", "movie=1"])
+    
+    time.sleep(video_length)
+    
+    for port in camera_ports:
+        subprocess.call(["gphoto2", "--port=" + port, "--set-config", "movie=0"])
 
-time.sleep(video_length)
-
-for port in camera_ports:
-    subprocess.call(["gphoto2", "--port=" + port, "--set-config", "movie=0"])
+    x = x + 1
 
