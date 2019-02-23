71d0be206b65669360c8870dedc22429ac6cd336
Ryan Jackson <jcpoulin@ualberta.ca>
2019-02-23 12:22:49 -0700

    Stitching through Wine is working!

--- video_stitch.py
+++ video_stitch.py
@@ -12,31 +12,37 @@ import subprocess
 import os
 
 # This function is for stitching all videos beyond the first one
-def video_stitch(x, path, log_file):
+def video_stitch(x, path_to_jpg, path_to_mp4, log_file):
     try:
         image_number = "%06d" % (x+1)
 
         # take the next stitched image and make it into the newest-video-frame
-        subprocess.call(["ffmpeg", "-y", "-framerate", "24", "-i", "/home/ryan/Documents/full_circle/stitchwatch/" + image_number + "-A.jpg", "-s", "2048x1024", "-vcodec", "libx264", "-cmp", "22", path + "newest-video-frame.mp4"])
+        subprocess.call(["ffmpeg", "-y", "-framerate", "24", "-i", path_to_jpg + image_number + "-A.jpg", "-s", "2048x1024", "-vcodec", "libx264", "-cmp", "22", path_to_mp4 + "newest-video-frame.mp4"])
 
         # take the current full-stitched-video and add the newest-video-frame to the end of it.
-        subprocess.call(["ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", "/home/ryan/Documents/full_circle/stitchwatch/input_files.txt", "-c", "copy", path + "new-full-stitched-video.mp4"])
+        subprocess.call(["ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", "/home/ryan/Documents/full_circle/stitchwatch/input_files.txt", "-c", "copy", path_to_mp4 + "new-full-stitched-video.mp4"])
 
         # over write the full-stitched-video with the new-full-stitched-video
-        subprocess.call(["mv", "-f", path + "new-full-stitched-video.mp4", path + "full-stitched-video.mp4"])
+        subprocess.call(["mv", "-f", path_to_mp4 + "new-full-stitched-video.mp4", path_to_mp4 + "full-stitched-video.mp4"])
         # repeat as new stitched images come in
-        pts_filename = "/home/ryan/Documents/full_circle/stitchwatch/" + image_number + "-A.pts"
-        txt_filename = "/home/ryan/Documents/full_circle/stitchwatch/" + image_number + "-A.txt"
-
-        print("Pts filename: ", pts_filename, "\tTxt filename: ", txt_filename)
-        subprocess.call(["mv", "-f", pts_filename, txt_filename])
-    except(e):
+        # pts_filename = "/home/ryan/Documents/full_circle/stitchwatch/" + image_number + "-A.pts"
+        # txt_filename = "/home/ryan/Documents/full_circle/stitchwatch/" + image_number + "-A.txt"
+        #
+        # print("Pts filename: ", pts_filename, "\tTxt filename: ", txt_filename)
+        # subprocess.call(["mv", "-f", pts_filename, txt_filename])
+        filepath = path_to_jpg + log_file
+        log_file = open(filepath, "a+")
+        message = "Attached new frame to video: " + image_number + "\n"
+        log_file.write(message)
+        log_file.close()
+    except:
         # Print the error to the terminal and to the log file
         print("\n\nError in attaching new frame to video:")
         print(e)
-        log_file = open(filename, "a+")
-        log_file.write("Error in attaching new frame to video: \n")
-        log_file.write(str(e) + '\n\n')
+        filepath = path_to_jpg + log_file
+        log_file = open(filepath, "a+")
+        log_file.write("Error in attaching new frame to video \n")
+        # log_file.write(str(e) + '\n\n')
         log_file.close()
 
 # This function is for stitching the first video
@@ -44,7 +50,7 @@ def first_stitch(path_to_jpg, path_to_mp4, log_file):
     try:
         # take first stitched  image and call it full-stitched-video
         subprocess.call(["ffmpeg", "-y", "-framerate", "24", "-i", path_to_jpg + "000001-A.jpg", "-s", "2048x1024", "-vcodec", "libx264", "-cmp", "22", path_to_mp4 + "full-stitched-video.mp4"])
-        subprocess.call(["mv", "-f", "/home/ryan/Documents/full_circle/stitchwatch/000001-A.pts", "/home/ryan/Documents/full_circle/stitchwatch/000001-A.txt"])
+        # subprocess.call(["mv", "-f", "/home/ryan/Documents/full_circle/stitchwatch/000001-A.pts", "/home/ryan/Documents/full_circle/stitchwatch/000001-A.txt"])
     except AttributeError as e:
         # Print the error to the terminal and to the log file
         print("\n\nError in attaching new frame to video:")
--- wine_stitch.py
+++ wine_stitch.py
@@ -21,16 +21,9 @@ try:
         txt_file_check = txt_file_check.decode('utf-8')
         txt_file_check = txt_file_check.splitlines()
 
-        txt_file = "%06d" % (x) + "-A.txt"
-
-        while not txt_file in txt_file_check:
-            txt_file_check = subprocess.check_output((["ls", full_dir_name]))
-            txt_file_check = txt_file_check.decode('utf-8')
-            txt_file_check = txt_file_check.splitlines()
-
         old_number = "%06d" % (x)
         new_number = "%06d" % (x+1)
-        old_path = full_dir_name + old_number + "-A.txt"
+        old_path = full_dir_name + old_number + "-A.pts"
         new_path = full_dir_name + new_number + "-A.pts"
         command = "sed 's/%s/%s/g' %s > %s" % (old_number, new_number, old_path, new_path)
         process = subprocess.call([command], shell=True)
@@ -47,18 +40,21 @@ except NameError as e:
 except AttributeError as e:
     print(e)
 
+# Run wine and wait for it to finish
 stitch_file = "%06d" % (x+1) + "-A.pts"
 process = subprocess.Popen(["wine", "/home/ryan/Desktop/PTGui.exe", "-batch", "-x", stitch_file])
-process.wait()
+# process.wait()
+time.sleep(40)
 
 # Update the current video, if it exists
 if x > 0:
-    video_stitch(x, "/home/ryan/Documents/full_circle/" + str(dir_name) + "_preview/", filename)
+    video_stitch(x, "/home/ryan/Documents/full_circle/" + str(dir_name) + "/", "/home/ryan/Documents/full_circle/" + str(dir_name) + "_preview/", filename)
 elif x == 0:
-    first_stitch("/home/ryan/Documents/full_circle/stitchwatch/", "/home/ryan/Documents/full_circle/" + str(dir_name) + "_preview/", filename)
+    first_stitch("/home/ryan/Documents/full_circle/" + str(dir_name) + "/", "/home/ryan/Documents/full_circle/" + str(dir_name) + "_preview/", filename)
 
 # Log the success
+jpg_name = "%06d" % (x+1) + "-A.jpg"
 log_file = open(filename, "a+")
-message = "\nStitched " + jpg_name + "\n"
+message = "Stitched " + jpg_name + "\n\n"
 log_file.write(message)
 log_file.close()
