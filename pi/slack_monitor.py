3b5dbfcdedb41e294e2552a8cd8de116644ade4f
Jolene Poulin <jcpoulin@ualberta.ca>
2019-03-21 17:42:57 -0600

    Fixed bugs with killbot

--- pi/slack_monitor.py
+++ pi/slack_monitor.py
@@ -4,28 +4,28 @@ import json
 import psutil
 from slackclient import SlackClient
 import subprocess
+import os
 
-slack_client = SlackClient("xoxb-29833002065-571970030258-zxnDkNQ3n2HDUKXG6lo2GCdd")
+slack_client = SlackClient("xoxb-29833002065-571970030258-j4tDeRh3RIy5ftM5QTDUs109")
 
 
 # Fetch your Bot's User ID
 user_list = slack_client.api_call("users.list")
-for user in user_list.get('members'):
-    if user.get('name') == "killbot5000":
-        slack_user_id = user.get('id')
-        break
+if user_list['ok']:
+    for user in user_list.get('members'):
+        if user.get('name') == "killbot5000":
+            slack_user_id = user.get('id')
+            break
 
 
 # Start connection
 if slack_client.rtm_connect():
-    print "Connected!"
+    print ("Connected!")
 
     while True:
         for message in slack_client.rtm_read():
             if 'text' in message and message['text'].startswith("<@%s>" % slack_user_id):
-                process = subprocess.Popen(["python", "/home/pi/Documents/full_circle/pi/killswitch_engage.py"])
-                process.wait()
-
+                
                 print "Message received: %s" % json.dumps(message, indent=2)
 
                 message_text = message['text'].\
@@ -33,12 +33,27 @@ if slack_client.rtm_connect():
                     strip()
 
                 if re.match(r'.*(engage).*', message_text, re.IGNORECASE):
+                    print("engage")
+                    process = subprocess.Popen(["python", "/home/pi/Documents/full_circle/pi/killswitch_engage.py"])
+                    process.wait()
 
                     slack_client.api_call(
                         "chat.postMessage",
                         channel=message['channel'],
                         text="The killswitch was engaged",
                         as_user=True)
+                        
+                if re.match(r'.*(you there?).*', message_text, re.IGNORECASE):
+                    print("you there?")
+
+                    slack_client.api_call(
+                        "chat.postMessage",
+                        channel=message['channel'],
+                        text="Hello!",
+                        as_user=True)
 
 
         time.sleep(1)
+        
+else:
+    print("HELP")
