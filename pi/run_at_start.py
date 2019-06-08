edab064ee130422f81a4dd5a7538cc8f26278c0b
Jo <jcpoulin@ualberta.ca>
2019-06-08 15:38:32 -0600

    Updated run at start functions

--- pi/run_at_start.py
+++ pi/run_at_start.py
@@ -6,5 +6,4 @@ import time
 subprocess.call(["python", "/home/pi/Documents/full_circle/pi/close_current.py"])
 time.sleep(5)
 
-subprocess.Popen(["python", "/home/pi/Documents/full_circle/pi/voltage_monitor.py"])
 subprocess.Popen(["python", "/home/pi/Documents/full_circle/pi/slack_monitor.py"])
